ce2f09cccc9d95e01bdc5dba2c36084821725c8d
cheymos <nevesomov@gmail.com>
2022-01-14 18:38:50 +0600

    refactor!: change file property type to local file

--- src/modules/post/entities/post-image.entity.ts
+++ src/modules/post/entities/post-image.entity.ts
@@ -1,24 +1,25 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToOne } from 'typeorm';
 import { BaseEntity } from '../../../common/entities/base.entity';
+import { LocalFile } from '../../local-file/local-file.entity';
 import { Post } from './post.entity';
 
 @ObjectType()
 @Entity()
 export class PostImage extends BaseEntity {
-  @Field()
-  @Column()
-  file: string;
+  @Field(() => LocalFile)
+  @OneToOne(() => LocalFile)
+  file: LocalFile;
 
   @Field(() => Post)
-  @ManyToOne(() => Post, post => post.images)
+  @ManyToOne(() => Post, (post) => post.images)
   post?: Post;
 
   @Field(() => Int)
   @Column()
   postId: number;
 
-  constructor(file: string, postId: number) {
+  constructor(file: LocalFile, postId: number) {
     super();
     this.file = file;
     this.postId = postId;
