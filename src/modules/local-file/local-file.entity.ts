a35d922ea88a916a8ca8c827ba34ddab6f906184
cheymos <nevesomov@gmail.com>
2022-01-15 01:13:27 +0600

    fix: remove local file entity from schema gql

--- src/modules/local-file/local-file.entity.ts
+++ src/modules/local-file/local-file.entity.ts
@@ -1,15 +1,11 @@
-import { Field, ObjectType } from '@nestjs/graphql';
 import { Column, Entity } from 'typeorm';
 import { BaseEntity } from '../../common/entities/base.entity';
 
-@ObjectType('File')
 @Entity('local_files')
 export class LocalFile extends BaseEntity {
-  @Field()
   @Column()
   filename: string;
 
-  @Field()
   @Column()
   path: string;
 
--- src/modules/post/entities/post-image.entity.ts
+++ src/modules/post/entities/post-image.entity.ts
@@ -1,27 +1,27 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { Column, Entity, ManyToOne, OneToOne } from 'typeorm';
+import { Field, HideField, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, ManyToOne } from 'typeorm';
 import { BaseEntity } from '../../../common/entities/base.entity';
-import { LocalFile } from '../../local-file/local-file.entity';
 import { Post } from './post.entity';
 
 @ObjectType()
 @Entity()
 export class PostImage extends BaseEntity {
-  @Field(() => LocalFile)
-  @OneToOne(() => LocalFile)
-  file: LocalFile;
+  @Field()
+  @Column()
+  filePath: string;
 
-  @Field(() => Post)
-  @ManyToOne(() => Post, (post) => post.images)
+  @HideField()
+  @ManyToOne(() => Post, { onDelete: 'CASCADE' })
   post?: Post;
 
-  @Field(() => Int)
+  @HideField()
   @Column()
   postId: number;
 
-  constructor(file: LocalFile, postId: number) {
+  constructor(filePath: string, postId: number) {
     super();
-    this.file = file;
+
+    this.filePath = filePath;
     this.postId = postId;
   }
 }
