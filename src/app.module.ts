73ace3f37df5ed437fae1229731656152824d7ac
cheymos <nevesomov@gmail.com>
2022-01-14 15:17:10 +0600

    build: add nest graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,16 @@ import { AppService } from './app.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './common/jwt.strategy';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, JwtStrategy],
