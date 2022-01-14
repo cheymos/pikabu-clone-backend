8fb9e2aa11b7bc4f8f1550f9a4a85bc8f9293ff7
cheymos <nevesomov@gmail.com>
2022-01-14 17:05:31 +0600

    fix: adapt auth guard to graphql

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -1,5 +1,23 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard as PassportAuthGuard } from '@nestjs/passport';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { AuthGuard as AG } from '@nestjs/passport';
+import { AuthenticationError } from 'apollo-server-express';
 
 @Injectable()
-export class AuthGuard extends PassportAuthGuard('jwt') {}
+export class AuthGuard extends AG('jwt') {
+  canActivate(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    const { req } = ctx.getContext();
+
+    return super.canActivate(new ExecutionContextHost([req]));
+  }
+
+  override handleRequest(err: any, user: any) {
+    if (err || !user) {
+      throw err || new AuthenticationError('Not authenticated');
+    }
+
+    return user;
+  }
+}
