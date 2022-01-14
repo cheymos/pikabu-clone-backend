6c298a62ad1fe9a356ec4981862188cd9848b95b
cheymos <nevesomov@gmail.com>
2022-01-14 18:32:13 +0600

    feat: create split between auth guard and gql auth guard

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -1,23 +1,3 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
-import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
-import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard as AG } from '@nestjs/passport';
-import { AuthenticationError } from 'apollo-server-express';
 
-@Injectable()
-export class AuthGuard extends AG('jwt') {
-  canActivate(context: ExecutionContext) {
-    const ctx = GqlExecutionContext.create(context);
-    const { req } = ctx.getContext();
-
-    return super.canActivate(new ExecutionContextHost([req]));
-  }
-
-  override handleRequest(err: any, user: any) {
-    if (err || !user) {
-      throw err || new AuthenticationError('Not authenticated');
-    }
-
-    return user;
-  }
-}
+export class AuthGuard extends AG('jwt') {}
---
+++ src/common/guards/gql-auth.guard.ts
@@ -0,0 +1,23 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { AuthGuard as AG } from '@nestjs/passport';
+import { AuthenticationError } from 'apollo-server-express';
+
+@Injectable()
+export class GqlAuthGuard extends AG('jwt') {
+  canActivate(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    const { req } = ctx.getContext();
+
+    return super.canActivate(new ExecutionContextHost([req]));
+  }
+
+  override handleRequest(err: any, user: any) {
+    if (err || !user) {
+      throw err || new AuthenticationError('Not authenticated');
+    }
+
+    return user;
+  }
+}
