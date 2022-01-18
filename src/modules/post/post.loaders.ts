11e5d32412104d332eff68b48560fc929c289011
cheymos <nevesomov@gmail.com>
2022-01-18 17:35:57 +0600

    feat: create loader for batch loading of tags

--- src/modules/post/post.loaders.ts
+++ src/modules/post/post.loaders.ts
@@ -1,11 +1,15 @@
 import { Injectable, Scope } from '@nestjs/common';
 import * as DataLoader from 'dataloader';
-import { PostImage } from './entities';
+import { PostImage, PostTag } from './entities';
 import { PostImageService } from './services/post-image.service';
+import { PostTagService } from './services/post-tag.service';
 
 @Injectable({ scope: Scope.REQUEST })
 export class PostLoaders {
-  constructor(private readonly postImageService: PostImageService) {}
+  constructor(
+    private readonly postImageService: PostImageService,
+    private readonly postTagService: PostTagService,
+  ) {}
 
   readonly batchImages = new DataLoader(async (postIds: number[]) => {
     const images = await this.postImageService.getByPostIds(postIds);
@@ -21,5 +25,21 @@ export class PostLoaders {
     });
 
     return postIds.map((id) => postIdToImages[id] || []);
+  });
+
+  readonly batchTags = new DataLoader(async (tagIds: number[]) => {
+    const tags = await this.postTagService.getByPostIds(tagIds);
+
+    const postIdToTags: { [key: string]: PostTag[] } = {};
+
+    tags.forEach((tag) => {
+      if (!postIdToTags[tag.postId]) {
+        postIdToTags[tag.postId] = [tag];
+      } else {
+        postIdToTags[tag.postId].push(tag);
+      }
+    });
+
+    return tagIds.map((id) => postIdToTags[id] || []);
   });
 }
