d910d8f1140e46910689e5e4dfcbe83410d30c0f
cheymos <nevesomov@gmail.com>
2022-01-26 21:12:29 +0600

    refactor!: change post sort enum

--- src/modules/post/enums/post-sort.enum.ts
+++ src/modules/post/enums/post-sort.enum.ts
@@ -1,10 +1,10 @@
 import { registerEnumType } from '@nestjs/graphql';
 
 export enum PostSort {
-  DATE_ASC,
-  DATE_DESC,
-  LIKE_ASC,
-  LIKE_DESC,
+  createdAt_ASC,
+  createdAt_DESC,
+  likes_ASC,
+  likes_DESC,
 }
 
 registerEnumType(PostSort, { name: 'PostSort' });
